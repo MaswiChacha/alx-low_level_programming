c78e4a297d519f776212c7d55399358afcfba888
MaswiChacha <maswichacha04@gmail.com>
2022-07-04 14:04:09 -0700

    last two

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/*include is the pre processor*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/*main is the entry point*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
