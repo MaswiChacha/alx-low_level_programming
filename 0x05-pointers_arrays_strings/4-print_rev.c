f5630caeb89b72360e550760b82b17fff5bc8798
MaswiChacha <maswichacha04@gmail.com>
2022-07-11 14:53:30 -0700

    changed s to c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,17 @@
 
 /**
  * print_rev - Prints a string in reverse.
- * @s: The string to be printed.
+ * @c: The string to be printed.
  */
-void print_rev(char *s)
+void print_rev(char *c)
 {
 	int len = 0, index;
 
-	while (s[index++])
+	while (c[index++])
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+		_putchar(c[index]);
 
 	_putchar('\n');
 }
