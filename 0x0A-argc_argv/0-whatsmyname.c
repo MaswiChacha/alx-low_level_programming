d3010dd509b906c8986fb891413fc8f08f3d54bf
MaswiChacha <maswichacha04@gmail.com>
2022-07-22 19:58:49 -0700

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - the entry point@argc:
- * argument counter
+ * main - the entry point
+ * @argc: argument counter
  * @argv: array of pointer to strings
  *
  * Return: 0
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+int i;
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
